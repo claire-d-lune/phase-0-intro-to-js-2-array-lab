00029c3528166ef60a39e3ab66007ac8cd18d46c
Claire Steinhoff <claire.steinhoff@gmail.com>
2022-09-20 11:19:52 -0600

    commit tree e45ba6abdf4b8eb59f6ea63fcba1f40a2d82373c

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat (name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat () {
+    cats.pop();
+    return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    return cats;
+}
+
+function appendCat(name) {
+    let moreCatsEnd = [...cats];
+    moreCatsEnd.push(name);
+    return moreCatsEnd;
+}
+
+function prependCat(name) {
+    let moreCatsFirst = [...cats];
+    moreCatsFirst.unshift(name);
+    return moreCatsFirst
+}
+
+function removeLastCat () {
+    let oneLessCat = [...cats];
+    oneLessCat.pop();
+    return oneLessCat;
+}
+
+function removeFirstCat () {
+    let oneLessCatFront = [...cats];
+    oneLessCatFront.shift();
+    return  oneLessCatFront;
+}
